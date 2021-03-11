9fa3b044f35546a9ec263e4e1eae158c106d384d
rsmith <legendary.belief@gmail.com>
2021-03-11 11:45:20 -0800

    commit tree 75003f0cfcfb22f34c837d119418bcaf8ffd0a10

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,21 @@
 
+#Methods; title, author, page count
+class Book
+    attr_accessor :title, :page_count, :genre
 
+    def initialize(title)
+        @title = title
+    end
+
+    def author=(author)
+        @author = author
+    end
+
+    def author
+        @author
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
